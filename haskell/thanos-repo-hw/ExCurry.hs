1c6f766fb8dd0e8d4f96e4288f53b413cc483431
Yasmin Giordano <giordanoyasmin06@gmail.com>
2024-10-21 07:46:39 -0300

    adição no ExCurry

--- haskell/thanos-repo-hw/ExCurry.hs
+++ haskell/thanos-repo-hw/ExCurry.hs
@@ -7,10 +7,10 @@ import Prelude hiding ( curry , uncurry )
 -- curry gets a "traditional" binary function
 -- and returns its currified version
 curry :: ((a, b) -> c) -> (a -> b -> c)
-curry = undefined
+curry f = \x y -> f (x, y)
 
 -- uncurry gets a currified function
 -- and returns its "traditional" binary version
 uncurry :: (a -> b -> c) -> ((a, b) -> c)
-uncurry = undefined
+uncurry f = \(x, y) -> f x y
 
