5cbff96cfd94b6c5dfca1bc12d269263ae9a740f
Yasmin Giordano <giordanoyasmin06@gmail.com>
2024-10-27 06:19:30 -0300

    monitoria lean

---
+++ haskell/thanos-repo-hw/ExHyper.hs
@@ -0,0 +1,63 @@
+module ExHyper where
+
+import Prelude hiding ( exp )
+
+-- Nat datatype --------------------------------
+
+data Nat = O | S Nat
+     deriving (Eq, Show)
+
+instance (Num Nat) where
+    (+) = add
+    (*) = mul
+    abs = id
+    fromInteger 0 = O
+    fromInteger n
+      | n > 0     = S $ fromInteger (n-1)
+      | otherwise = O
+    signum O = O
+    signum n = S O
+    negate n = O
+
+instance (Ord Nat) where
+    O     <= m     = True
+    (S n) <= O     = False
+    (S n) <= (S m) = n <= m
+
+-- explicit definitions of add, mul, exp:
+
+add n O     = n
+add n (S m) = S (add m n)
+
+mul n O     = O
+mul n (S m) = add (mul n m) n
+
+exp n O     = S O
+exp n (S m) = mul (exp n m) n
+
+
+
+------------------------------------------------
+
+-- substitute 'undefined' by the correct number
+-- to define each of those functions:
+
+add :: Nat -> Nat -> Nat
+add = hyper 1
+
+mul :: Nat -> Nat -> Nat
+mul = hyper 2
+
+exp :: Nat -> Nat -> Nat
+exp = hyper 3
+
+-- hyper n should return the n'th operation in the sequence:
+-- (..?..), add, mul, exp, ...?
+
+hyper :: Integral i => i -> (Nat -> Nat -> Nat)
+hyper n
+    | n == 1 = add
+    | n == 2 = mul
+    | n == 3 = exp
+    | otherwise = error "hyper: no operation for this index"
+
